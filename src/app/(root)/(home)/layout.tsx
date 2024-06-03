32061711c78851e7c2ce48c2b86b59d5698d4a51
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-03 19:51:04 +0530

    refactor: Update home layout styling for responsiveness

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -13,10 +13,10 @@ export const metadata: Metadata = {
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="relative">
+    <main className="relative flex flex-col min-h-screen max-w-screen">
       <Navbar />
-      <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
-        <div className="w-full">{children}</div>
+      <section className="flex flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14 flex-1">
+        <div className="max-w-full">{children}</div>
       </section>
       <Footer />
     </main>
